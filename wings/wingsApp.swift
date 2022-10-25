6e7255a07210c3820f476f04bf061d776d8dd656
bram <dasuqiibrohim@gmail.com>
2022-10-25 09:59:38 +0700

    Commit first.

---
+++ wings/wingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  wingsApp.swift
+//  wings
+//
+//  Created by ACI 2 on 25/10/22.
+//
+
+import SwiftUI
+
+@main
+struct wingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
